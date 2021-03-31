ecc65d2673d8d3dc47f57c0290ce59ec3818f4be
Josh Greenberg <castro.greenberg@gmail.com>
2021-03-30 19:45:12 -0500

    Add route to test @login_required decorator.

--- server/api/home_handler.py
+++ server/api/home_handler.py
@@ -1,7 +1,16 @@
 from flask import jsonify, Blueprint
-home_handler = Blueprint('home_handler', __name__)
 
+from models import User
+from flask_login import login_required
+
+home_handler = Blueprint('home_handler', __name__)
 
 @home_handler.route('/welcome')
 def welcome():
-    return jsonify({'welcomeMessage': 'Welcome!'})
+    users = User.objects()
+    return jsonify({'welcomeMessage': 'Welcome!', "users": users})
+
+@home_handler.route('/welcome-protected')
+@login_required
+def welcome_protected():
+    return jsonify({"welcomeMessage": "Welcome to the login protected area!"})
